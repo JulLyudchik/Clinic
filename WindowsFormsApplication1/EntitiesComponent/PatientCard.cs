938f3dabf250e3f4415b5dfb50bb7781e564bc1f
varchenkoandrey2015 <varchenkoandrey2015@yandex.ru>
2016-12-23 01:14:29 +0300

    ещё

---
+++ WindowsFormsApplication1/EntitiesComponent/PatientCard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class PatientCard:Entity
+    {
+        public string name { get; set; }
+        public DateTime creationDate { get; set; }
+        public DateTime birthDate { get; set; }
+        public string sex { get; set; }
+        public Street street { get; set; }
+        public string houseApartment { get; set; }
+        public RegStation registrationStation { get; set; }
+        public List<Visit> visits { get; set; }
+    }
+}
---
+++ WindowsFormsApplication1/EntitiesComponent/Visit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Visit:Entity
+    {
+        public DateTime date { get; set; }
+        public Specialization speciazation { get; set; }
+        public Doctor doctor { get; set; }
+        public List<Drug> drugs { get; set; }
+        public List<Diagnosis> diagnoses { get; set; }
+    }
+}
